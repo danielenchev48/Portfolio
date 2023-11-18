5f04a1c0a4f52fcf7ca4d5f42107c9b801ac7c3b
DanielEnchev48 <neroenchev@gmail.com>
2023-11-18 15:41:03 +0200

    Icons added

--- components/mainPage/about/about.jsx
+++ components/mainPage/about/about.jsx
@@ -1,3 +1,6 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function About() {
@@ -6,14 +9,14 @@ export default function About() {
             <div className="headerTitle">About me</div>
             <div className="information">
                 <div className="section">
-                    <div className="header">Background</div>
+                    <div className="header"><FontAwesomeIcon icon={faUser} className="me-2" /> Background</div>
                     <p>Working with computers since youth and recently completed the full course for Junior JavaScript Developer
                         in Advance Academy. Now after working in a group project for 4 months I have a solid foundation of the
                         stages in developing websites and applications.
                     </p>
                 </div>
                 <div className="section">
-                    <div className="header">Availability</div>
+                    <div className="header"><FontAwesomeIcon icon={faLaptopCode} className="me-2" /> Availability</div>
                     <p>Now I search for a place to keep growing and learn new skills while working every day and improving
                         my own projects, because staying in one place and not working on yourself is incorrect.
                         <span className="fst-italic"> /...and leads to console errors/</span>
--- components/mainPage/ethics/ethics.jsx
+++ components/mainPage/ethics/ethics.jsx
@@ -1,3 +1,5 @@
+import { faFileCode, faLightbulb, faWindowRestore } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Ethics() {
     return <>
@@ -5,17 +7,17 @@ export default function Ethics() {
             <div className="headerTitle">Ethics</div>
             <div className="principles">
                 <div className="section">
-                    <h2>Responsive Design</h2>
+                    <h2><FontAwesomeIcon icon={faWindowRestore} className="me-2" /> Responsive Design</h2>
                     <p>Today a website can be opened from a majority of different devices so
                         responsiveness is importand part of developing an accessible application for everyone.</p>
                 </div>
                 <div className="section">
-                    <h2>Clean Code</h2>
+                    <h2><FontAwesomeIcon icon={faFileCode} className="me-2" /> Clean Code</h2>
                     <div>Code is clean if it can be understood easily – by everyone on the team.
                         With understandability comes readability, changeability, extensibility and maintainability.</div>
                 </div>
                 <div className="section">
-                    <h2>Quick Learning</h2>
+                    <h2><FontAwesomeIcon icon={faLightbulb} className="me-2" /> Quick Learning</h2>
                     <div>In the Computer Age that we live in technologies are evolving faster and faster,
                         being able to adapt to new situations is key in staying relevant as a developer.</div>
                 </div>
